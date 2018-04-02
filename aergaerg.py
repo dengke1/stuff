00a4df6dcf355cab367ac098f47ce639418284c4
dengke1 <k.deng123@gmail.com>
2018-04-02 15:38:53 -0400

    snake needs to grow

--- aergaerg.py
+++ aergaerg.py
@@ -9,6 +9,9 @@ gmap = [[0 for x in range(w)] for y in range(h)]
 blue = Color(0, 0, 255)
 black = Color(0, 0, 0)
 red = Color(255, 0, 0)
+sMaxSpeed = 0.035
+sMinSpeed = 0.06
+
 
 class pc(pygame.sprite.Sprite):
 	'''
@@ -102,25 +105,23 @@ class segment(pygame.sprite.Sprite):
 
 	def move(self):
 		'''
-		Moves segment to location where previous segment was
-		NOTE: DO NOT CALL ON HEAD
+		Moves segment to location where previous segment was, updates map values
 		'''
-		temp = self.getPos()
 		if self.getNext() is None:
-			gmap[temp[1]][temp[0]] = 0
-		next = self.getPrev()
-		nextc = next.getPos()
-		self.xpos = nextc[0]
-		self.ypos = nextc[1]
-		self.rect = next.rect
-		gmap[ypos][xpos] = 8
+			gmap[self.ypos][self.xpos] = 0
+		prev = self.getPrev()
+		prevc = prev.getPos()
+		self.xpos = prevc[0]
+		self.ypos = prevc[1]
+		self.rect = prev.rect
+		gmap[self.ypos][self.xpos] = 8
 
 
 class snake(pygame.sprite.Sprite):
 	'''
 	8 = snake
 	'''
-	hx = w-3
+	hx = w-4
 	hy = h-1
 	direction = 'w'
 
@@ -129,12 +130,16 @@ class snake(pygame.sprite.Sprite):
 		self.head = segment(self.hx, self.hy)
 		one = segment(self.hx + 1, self.hy)
 		two = segment(self.hx + 2, self.hy)
+		three = segment(self.hx + 3, self.hy)
 
 		self.head.setNext(one)
 		one.setNext(two)
+		two.setNext(three)
+
 		one.setPrev(self.head)
 		two.setPrev(one)
-		self.tail = two
+		three.setPrev(two)
+		self.tail = three
 
 
 	def onwards(self):
@@ -150,9 +155,11 @@ class snake(pygame.sprite.Sprite):
 		elif self.direction == 'e':
 			self.head.xpos+=1
 			self.head.rect = self.head.rect.move(blocksize, 0)
+
 		if gmap[self.head.ypos][self.head.xpos] == 1:
 			return True
 		else:
+			gmap[self.head.ypos][self.head.xpos] = 8
 			return False
 
 	def north(self):
@@ -167,11 +174,27 @@ class snake(pygame.sprite.Sprite):
 	def east(self):
 		self.direction = 'e'
 
-	def chase(self, x, y):
+	def chase(self, x, y, grow):
 		xdif = abs(x - self.head.xpos)
 		ydif = abs(y - self.head.ypos)
 
-		ret = self.onwards();
+		# move head and rest of body from tail up
+		temp = self.tail
+		gmap[temp.ypos][temp.xpos] = 0
+		while temp != self.head:
+			prev = temp.getPrev()
+			cprev = prev.getPos()
+
+			temp.xpos = cprev[0]
+			temp.ypos = cprev[1]
+
+			temp.rect = prev.rect
+			temp = prev
+
+		ret = self.onwards()
+
+
+		# Change directions based on location
 		if xdif <= 1:
 			if y > self.head.ypos:
 				self.south()
@@ -185,7 +208,16 @@ class snake(pygame.sprite.Sprite):
 				self.west()
 		return ret
 
+def drawSnake(seg, dis, color):
+	while seg is not None:
+		pygame.draw.rect(dis, color, seg.rect)
+		seg = seg.getNext()
+
+
 def main():
+	count = 10
+	doGrow = False
+
 	pygame.init()
 	screen = pygame.display.set_mode((w*blocksize, h*blocksize), 0, 32)
 	player = pc()
@@ -193,10 +225,7 @@ def main():
 
 	pygame.draw.rect(screen, blue, player.rect)
 	
-	i = enemy.head
-	while i is not None:
-		pygame.draw.rect(screen, red, i.rect)
-		i = i.getNext()
+	drawSnake(enemy.head, screen, red)
 
 	pygame.key.set_repeat(1, 1)
 	keystate = pygame.key.get_pressed()
@@ -206,7 +235,7 @@ def main():
 	pcDelay = 0.03
 
 	sWalkCD = 0
-	sDelay = 0.03
+	sDelay = sMinSpeed
 	# main loop
 	while player.isalive:
 
@@ -219,9 +248,7 @@ def main():
 		pos = player.getPos()
 
 		i = enemy.head
-		while i is not None:
-			pygame.draw.rect(screen, red, i.rect)
-			i = i.getNext()
+		drawSnake(enemy.head, screen, red)
 
 		for event in pygame.event.get():
 			if event.type==QUIT or (event.type==KEYDOWN and keystate[K_q]):
@@ -230,6 +257,7 @@ def main():
 			if pcWalkCD <= 0:
 				if event.type==KEYDOWN:
 					pygame.draw.rect(screen, black, player.rect)
+
 					if keystate[K_UP]:
 						player.move(8)
 						pcWalkCD = pcDelay
@@ -253,10 +281,17 @@ def main():
 
 
 		if sWalkCD <= 0:
-			if enemy.chase(pos[0], pos[1]):
+			drawSnake(enemy.head, screen, black)
+			count -= 1
+			if enemy.chase(pos[0], pos[1], doGrow):
 				player.isalive = False
+				doGrow = False
+			if count == 0: 
+				count = 10
+				doGrow = True
+				if sDelay > sMaxSpeed:
+					sDelay -= 0.0005
 			sWalkCD = sDelay
-
 		pygame.display.update()
 
 

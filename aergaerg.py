21ee28985f4cf0d3c031ad32b3689d74757d6ddf
dengke1 <k.deng123@gmail.com>
2018-04-04 17:04:03 -0400

    better

--- aergaerg.py
+++ aergaerg.py
@@ -2,21 +2,22 @@ import sys, random
 import pygame
 from pygame.locals import *
 
-w = 50
-h = 37
+w = 30
+h = 20
 blocksize = 10
 gmap = [[0 for x in range(w)] for y in range(h)]
 blue = Color(0, 0, 255)
 black = Color(0, 0, 0)
 red = Color(255, 0, 0)
-sMaxSpeed = 0.0315
+sMaxSpeed = 0.0305
 sMinSpeed = 0.06
 playerSpeed = 0.03
+topBuffer = blocksize*3
 
 
 class pc(pygame.sprite.Sprite):
 	'''
-	square representing player
+	square representing player character
 	'''
 	xpos = 0
 	ypos = 0
@@ -25,10 +26,7 @@ class pc(pygame.sprite.Sprite):
 		pygame.sprite.Sprite.__init__(self)
 		gmap[self.ypos][self.xpos] = 1
 		self.isalive = True
-		self.rect = pygame.Rect(self.xpos, self.ypos, blocksize, blocksize)
-
-		# screen = pygame.display.get_surface()
-		# self.area = screen.get_rect()
+		self.rect = pygame.Rect(self.xpos + blocksize, self.ypos + topBuffer, blocksize, blocksize)
 
 	def getPos(self):
 		return (self.xpos, self.ypos)
@@ -87,7 +85,7 @@ class segment(pygame.sprite.Sprite):
 		self.xpos = x
 		self.ypos = y
 		gmap[self.ypos][self.xpos] = 8
-		self.rect = pygame.Rect(self.xpos*blocksize+1, self.ypos*blocksize+1, blocksize-2, blocksize-2)
+		self.rect = pygame.Rect(self.xpos*blocksize+blocksize+1, self.ypos*blocksize+topBuffer+1, blocksize-1, blocksize-1)
 
 	def getPos(self):
 		return [self.xpos, self.ypos]
@@ -166,11 +164,14 @@ class snake(pygame.sprite.Sprite):
 	def chase(self, x, y, grow):
 		'''
 		changes direction of snake and chases pc, also grows occasionally
+
+		there is a case where snake can make a 180 degree turn inside itself if pc
+		makes a 180 degree turn in 4 blocks
 		'''
 		xdif = abs(x - self.head.xpos)
 		ydif = abs(y - self.head.ypos)
 
-		# move head and rest of body from tail up
+		# move body from tail up
 		temp = self.tail
 		gpos = temp.getPos()
 		gmap[temp.ypos][temp.xpos] = 0
@@ -183,27 +184,43 @@ class snake(pygame.sprite.Sprite):
 
 			temp.rect = prev.rect
 			temp = prev
-		if grow:
+		if not grow:
 			newTail = segment(gpos[0], gpos[1])
 			self.tail.setNext(newTail)
 			newTail.setPrev(self.tail)
 			self.tail = newTail
-
+		# checks for pc collision and moves
 		ret = self.onwards()
 
 
 		# Change directions based on location
-		if xdif <= 1:
-			if y > self.head.ypos:
-				self.direction = 's'
-			else:
-				self.direction = 'n'
+		# if xdif <= 1:
+		# 	if y > self.head.ypos:
+		# 		self.direction = 's'
+		# 	else:
+		# 		self.direction = 'n'
 
-		if ydif <= 1:
-			if x > self.head.xpos:
-				self.direction = 'e'
-			else:
-				self.direction = 'w'
+		# elif ydif <= 1:
+		# 	if x > self.head.xpos:
+		# 		self.direction = 'e'
+		# 	else:
+		# 		self.direction = 'w'
+
+
+		#i have to do it this way or werid things happen
+		# (weird things still happen but at least in an expected manner)
+
+		if xdif <= 1 and y > self.head.ypos:
+			self.direction = 's'
+
+		if ydif <= 1 and x > self.head.xpos:
+			self.direction = 'e'
+
+		if xdif <= 1 and y < self.head.ypos:
+			self.direction = 'n'
+
+		if ydif <= 1 and x < self.head.xpos:
+			self.direction = 'w'
 		return ret
 
 def drawSnake(seg, dis, color):
@@ -211,7 +228,7 @@ def drawSnake(seg, dis, color):
 		pygame.draw.rect(dis, color, seg.rect)
 		seg = seg.getNext()
 
-def drawSnake2(seg, dis):
+def drawSnake2(seg, dis, color):
 	r = 255
 	g = 0
 	b = 0
@@ -230,25 +247,34 @@ def drawSnake2(seg, dis):
 		elif b <= 5 and r <= 250:
 			b = 0
 			r += 5
+		elif r >= 250 and b <= 250:
+			b += 5
 		pygame.draw.rect(dis, Color(r, g, b), seg.rect)
 		seg = seg.getNext()
 
 
 def game():
+	score = 0
 	count = 10
 	doGrow = False
 
 	pygame.init()
-	screen = pygame.display.set_mode((w*blocksize, h*blocksize))
+	screen = pygame.display.set_mode((w*blocksize + (2*blocksize), h*blocksize + topBuffer + blocksize))
 	player = pc()
 	enemy = snake()
 
-	pygame.draw.rect(screen, blue, player.rect)
-	
-	drawSnake(enemy.head, screen, red)
 
-	pygame.key.set_repeat(1, 1)
-	keystate = pygame.key.get_focused()
+	if pygame.font:
+		font = pygame.font.Font(None, 20)
+		text = font.render(str(score), 1, (200, 200, 200))
+		screen.blit(text, (w*blocksize, blocksize*1.5))
+		scoreBlock = pygame.Rect(w/2*blocksize, 0, (w+2)*blocksize, 3*blocksize-2)
+
+	border = pygame.Rect(blocksize-2, topBuffer-2, w*blocksize+4, h*blocksize+4)
+	pygame.draw.rect(screen, (200, 200, 200), border, 1)
+
+	pygame.draw.rect(screen, blue, player.rect)
+	drawSnake(enemy.head, screen, red)
 
 	clock = pygame.time.Clock()
 
@@ -257,9 +283,11 @@ def game():
 
 	sWalkCD = 0
 	sDelay = sMinSpeed
+
+	pygame.key.set_repeat(1, 1)
+	keystate = pygame.key.get_pressed()
 	# main loop
 	while player.isalive:
-
 		delta = clock.tick() / 1000.0
 		pcWalkCD -= delta
 		sWalkCD -= delta
@@ -268,55 +296,58 @@ def game():
 
 		pos = player.getPos()
 
-		drawSnake2(enemy.head, screen)
+		drawSnake2(enemy.head, screen, red)
 
 		for event in pygame.event.get():
 			if event.type==QUIT or (event.type==KEYDOWN and keystate[K_q]):
 				pygame.quit()
 				sys.exit()
-			if pcWalkCD <= 0:
-				if event.type==KEYDOWN:
-					pygame.draw.rect(screen, black, player.rect)
+			if pcWalkCD <= 0 and event.type==KEYDOWN:
+				pygame.draw.rect(screen, black, player.rect)
 
-					if keystate[K_UP]:
-						player.move(8)
-						pcWalkCD = pcDelay
-					elif keystate[K_DOWN]:
-						player.move(5)
-						pcWalkCD = pcDelay
-					elif keystate[K_LEFT]:
-						player.move(4)
-						pcWalkCD = pcDelay
-					elif keystate[K_RIGHT]:
-						player.move(6)
-						pcWalkCD = pcDelay
-					elif keystate[K_m]:
-						print("#####################################################")
-						for i in gmap:
-							for m in i:
-								print(m, end="")
-							print()
-					
+				if keystate[K_UP]:
+					player.move(8)
+					pcWalkCD = pcDelay
+				elif keystate[K_DOWN]:
+					player.move(5)
+					pcWalkCD = pcDelay
+				elif keystate[K_LEFT]:
+					player.move(4)
+					pcWalkCD = pcDelay
+				elif keystate[K_RIGHT]:
+					player.move(6)
+					pcWalkCD = pcDelay
+				elif keystate[K_m]:
+					print("#####################################################")
+					for i in gmap:
+						for m in i:
+							print(m, end="")
+						print()
+				
 		keystate = pygame.key.get_pressed()
-
 		# move snake, update counter to increase speed and length
 		if sWalkCD <= 0:
 			drawSnake(enemy.head, screen, black)
-			count -= 1
-			if enemy.chase(pos[0], pos[1], doGrow):
+			pygame.draw.rect(screen, black, scoreBlock)
+
+			#score updating
+			tscore = str(score)
+			placement = len(tscore)-2
+			score+=5
+			text = font.render(tscore, 1, (200, 200, 200))
+			screen.blit(text, ((w - placement)*blocksize, blocksize*1.5))
+
+			count-=1
+			if enemy.chase(pos[0], pos[1], count):
 				player.isalive = False
-			doGrow = False
 			if count == 0: 
 				count = 10
-				doGrow = True
 				if sDelay > sMaxSpeed:
-					sDelay -= 0.0005
+					sDelay-=0.0005
 			sWalkCD = sDelay
 		pygame.display.update()
-	return 1
+	return score
 
-def end():
-	screen = pygame.display.set_mode((w*blocksize, h*blocksize), pygame.OPENGL)
 
 
 if __name__ == '__main__':
